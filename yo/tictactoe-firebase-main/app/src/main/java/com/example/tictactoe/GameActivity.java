bd6c506bc6f2be5bb83f8e64efeb2eff3495523b
Christoph Winkler <winkler3@edu.htl-villach.at>
2022-05-31 20:40:03 +0200

    changes

--- yo/tictactoe-firebase-main/app/src/main/java/com/example/tictactoe/GameActivity.java
+++ yo/tictactoe-firebase-main/app/src/main/java/com/example/tictactoe/GameActivity.java
@@ -21,9 +21,33 @@ import java.util.Date;
 import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
-    // Mit der Firebase cloud verbinden
+    // Initialize the Firebase instance
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://tictactoe-winksai-default-rtdb.europe-west1.firebasedatabase.app/");
     DatabaseReference reference;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     boolean kicked = false;
@@ -39,68 +63,70 @@ public class GameActivity extends AppCompatActivity {
 
     boolean gameActive = true;
 
-    // Wer sollte einen Move machen?
+    // Who shall make a move?
     // 0 - X
     // 1 - O
     int activePlayer = 0;
     int playerID = 0;
 
-    // Array, welcher den Status jedes Feldes speichert
+    // Array storing the state of each field.
     int[] fieldState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
-    // Feld-Belegung
+    // Field state info:
     // 0 - X
     // 1 - O
     // 2 - Empty
 
-    // alle Siegespositionen/kombinationen in einem array speichern
+    // Store all winning positions in a two-dimensional array.
+    // The board:
     //  0  1  2
     //  3  4  5
     //  6  7  8
     int[][] winPositions = {
-            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},    // horizontal
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},    // vertical
-            {0, 4, 8}, {2, 4, 6}                // diagonal
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},    // Horizontal positions
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},    // Vertical positions
+            {0, 4, 8}, {2, 4, 6}                // Diagonal positions
     };
-    // Eine Gewinnkombination wir gespeichert in winPosition[x]. Es gibt 8 Siegespositionen.
+    // A win position is stored as winPosition[x]. There are 8 winning positions in total.
+    // To get each field of a single winPosition, winPosition[x][y] with 1 <= y <= 3 can be used.
+    // For tic tac toe, every single winning position can be defined by hand. Would be harder for a game like "4 Gewinnt".
 
-
-    // Spiel zurücksetzten bei Unentschieden
+    // For resetting the game on draw.
     public static int counter = 0;
 
-    // Funktion makeMove wird bei jedem Klick im Spielfeld ausgefüht
+    // This function is called at every click on an ImageView at the game activity.
     public void makeMove(View view) {
         Log.w("MainActivity", "Game ID: " + gameID);
 
-        // Blockieren des Spielers, wenn er nicht am Zug it
+        // Block move-making when the player is waiting for the turn of another player.
         if (activePlayer != playerID) {
             return;
         }
 
-        // Spiel zurücksetzten, wenn Sieg oder Unentschieden
+        // Reset the game on next click when a player won or the game is drawn.
         if (!gameActive) {
             resetGame();
         }
 
-        // ImageView.tag speichert die position id auf dem Game board (0-8)
+        // The ImageView.tag stores the position id on the game board from 0 to 8.
         ImageView img = (ImageView) view;
         int tappedImage = Integer.parseInt(img.getTag().toString());
 
         Log.w("MainActivity", "x: " + tappedImage);
 
-        // Nur eine Aktion zulassen wenn dieses Feld bereits noch nicht belegt ist
+        // Only proceed if the tapped image is empty.
         if (fieldState[tappedImage] == 2) {
-            // move-counter erhöhen
+            // Increase the current move counter.
             counter++;
 
-            // Bei 9 Aktionen mit keinem Gewinner -> Unentschieden
+            // If its the 9th move and no winner can be determined later, the game will be drawn.
             if (counter == 9) {
                 gameActive = false;
             }
 
             fieldState[tappedImage] = activePlayer;
 
-            // wechseln des aktiven Spielers (der Spieler, welcher am Zug ist)
+            // change the active player from 0 to 1 or from 1 to 0
             refreshImage(img, activePlayer);
 
             if (activePlayer == 0) {
@@ -109,7 +135,7 @@ public class GameActivity extends AppCompatActivity {
                 activePlayer = 0;
             }
 
-            // sieger flag
+            // winner flag
             int flag = checkGameWinner();
 
             List<Integer> fieldStateList = new ArrayList<>(fieldState.length);
@@ -126,7 +152,7 @@ public class GameActivity extends AppCompatActivity {
         gameActive = true;
         counter = 0;
 
-        // alle Felder zurücksetzten
+        // Reset all fields inside the grid.
         Arrays.fill(fieldState, 2);
 
         List<Integer> fieldStateList = new ArrayList<>(fieldState.length);
@@ -149,20 +175,19 @@ public class GameActivity extends AppCompatActivity {
 
     public int checkGameWinner() {
         int winningFlag = 0;
-        // Kontrollieren ob ein Spieler gewonnen hat
+        // Check if a player just won the game.
         for (int[] winPosition : winPositions) {
-
-            //schauen ob alle Felder 0 oder 1 sind
+            // Iterate through the winning positions and check if all three fields on the board are either 0 or 1 (player X or O).
             if (fieldState[winPosition[0]] != 2 &&
                     fieldState[winPosition[0]] == fieldState[winPosition[1]] &&
                     fieldState[winPosition[1]] == fieldState[winPosition[2]]) {
 
-                // Setzten der Gewinner-flag
+                // Set the winner flag.
                 winningFlag = 1;
 
                 String winnerStr;
 
-                // Spiel beim nächsten Zug zurücksetzten
+                // Reset the game on next tap.
                 gameActive = false;
 
                 if (fieldState[winPosition[0]] == 0) {
@@ -171,13 +196,13 @@ public class GameActivity extends AppCompatActivity {
                     winnerStr = "O hat das Spiel gewonnen";
                 }
 
-                // Updaten der Status Textleiste
+                // Update the status TextView text.
                 TextView status = findViewById(R.id.status);
                 status.setText(winnerStr);
             }
         }
 
-        // winnerflag auf unentschiden setzten, wenn 9 Aktionen ausgeführt wurden und es keine Sieger gibt
+        // If 9 moves were made and no winner is determined (flag was not set), the game is a draw.
         if (winningFlag == 0 && counter == 9) {
             TextView status = findViewById(R.id.status);
             status.setText("Spiel unentschieden");
@@ -191,13 +216,15 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // Der Spieler der das Spiel erstellte, bekommt die ID 0
+        // the player which created the game is player ID 0
         gameID = getIntent().getIntExtra("gameID", 0);
         playerID = getIntent().getIntExtra("playerID", 0);
 
+        // Display the game ID
+        TextView gameIdView = (TextView) findViewById(R.id.gameIdView);
+        gameIdView.setText("Game-ID: " + gameID);
 
-
-        // Anzeigen von Du spielst X bzw. 0
+        // Display whoAmI
         TextView whoAmI = findViewById(R.id.whoAmI);
         if (playerID == 0) {
             whoAmI.setText("Du spielst X");
@@ -209,13 +236,13 @@ public class GameActivity extends AppCompatActivity {
                 .child(String.valueOf(gameID));
 
         if (playerID == 0) {
-            // Wenn der Spieler das Spiel erstellt hat, erstellt er eine neue ArrayList, die alle 2 enthält, und überträgt sie an die Firebase.
+            // If the player created the game, created a new ArrayList containing all 2's and push it to Firebase.
             List<Integer> fieldStateList = new ArrayList<>(fieldState.length);
             for (int i : fieldState) {
                 fieldStateList.add(i);
             }
 
-            // Der Spieler, der das Spiel erstellt, macht die erste Aktion
+            // The player creating the game makes the first move.
             activePlayer = 0;
 
             reference.child("fieldState").setValue(fieldStateList);
@@ -238,7 +265,7 @@ public class GameActivity extends AppCompatActivity {
         } else {
             anyFieldSet = false;
 
-            // Vor den Joinen eines SPieles, zuerst schauen ob schon eine Aktion ausgeführt wurde
+            // On joining an existing game, first check if any field is already checked.
             reference.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
@@ -264,7 +291,7 @@ public class GameActivity extends AppCompatActivity {
 
                 @Override
                 public void onCancelled(DatabaseError error) {
-                    // Exeption wenn fehler beim laden
+                    // Failed to read value
                     Log.w("MainActivity", "Failed to read value.", error.toException());
                 }
             });
@@ -278,7 +305,7 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void listenForMoves() {
-        // Auf eine Aktion der Datenbank warten nach jede Zug
+        // Wait for an action from the database after every turn.
         moveListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -286,13 +313,13 @@ public class GameActivity extends AppCompatActivity {
                     resetGame();
                 }
 
-                // Firebase benöotigt ein Logfile, hier ist eine liste damit
+                // A List containing Longs is needed, as Firebase seems to return this.
                 Log.w("MainActivity", "Data change.");
                 List<Long> fieldStateList = (List<Long>) dataSnapshot.child("fieldState").getValue();
 
                 activePlayer = dataSnapshot.child("activePlayer").getValue(Integer.class);
 
-                // Game board refreshen
+                // Refresh the game board
                 refreshScene(fieldStateList);
 
                 isXActive = (Boolean) dataSnapshot.child("XisActive").getValue();
@@ -304,7 +331,7 @@ public class GameActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError error) {
-                // Fehler beim lesen
+                // Failed to read value
                 Log.w("MainActivity", "Failed to read value.", error.toException());
             }
         };
@@ -313,16 +340,16 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void refreshScene(List<Long> fieldStateList) {
-        // Arraylist mit lokalem int[] array vergleichen
-        // fieldStateList = neue field states
-        // fieldState = aktuelle lokale field state
+        // Compare the ArrayList from Firebase to the local int[] array.
+        // fieldStateList = new field states
+        // fieldState = current local field state
 
-        for(int i = 0; i < fieldState.length; i++) {  // läuft 9 mal
-            if (fieldStateList.get(i) != fieldState[i]) {  // wenn lokal und server nicht übereinstimmen
+        for(int i = 0; i < fieldState.length; i++) {  // running 9 times, for each tile
+            if (fieldStateList.get(i) != fieldState[i]) {  // if local and server state don't match
                 anyFieldSet = true;
-                ImageView img = findViewById(R.id.linearLayout).findViewWithTag(String.valueOf(i));
+                ImageView img = findViewById(R.id.linearLayout).findViewWithTag(String.valueOf(i));  // get the non-matching tile
 
-                // refreshen des lokalen states
+                // Refresh the local state at this position.
                 fieldState[i] = fieldStateList.get(i).intValue();
 
                 refreshImage(img, fieldState[i]);
@@ -336,17 +363,17 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void refreshImage(ImageView img, Integer playerID) {
-        // setzten des display image
+        // Set the displaying image accordingly.
         if (playerID == 0) {
             img.setImageResource(R.drawable.x);
 
-            // Ändern des TextBox Status
+            // Change the text of the status TextBox
             TextView status = findViewById(R.id.status);
             status.setText("Am Zug: O");
         } else {
             img.setImageResource(R.drawable.o);
 
-            // Ändern des TextBox Status
+            // Change the text of the status TextBox
             TextView status = findViewById(R.id.status);
             status.setText("Am Zug: X");
         }
@@ -363,7 +390,7 @@ public class GameActivity extends AppCompatActivity {
                 reference.child("OisActive").setValue(false);
             }
 
-            // Fehler beim lesen
+            // Failed to read value
             ValueEventListener listener = new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
